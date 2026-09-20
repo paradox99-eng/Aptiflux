111a1fbabd0ea8139e591859a9db64d2eca30e89
paradox99-eng <parthibdutta947@gmail.com>
2026-09-20 19:46:03 +0530

    Hardcode metadataBase to deployed URL to fix OG images

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -34,13 +34,7 @@ export const metadata = {
     address: false,
     telephone: false,
   },
-  metadataBase: new URL(
-    process.env.NEXT_PUBLIC_BASE_URL
-      ? process.env.NEXT_PUBLIC_BASE_URL
-      : process.env.VERCEL_URL
-      ? `https://${process.env.VERCEL_URL}`
-      : 'http://localhost:3000'
-  ),
+  metadataBase: new URL('https://aptiflux.vercel.app'),
   alternates: {
     canonical: '/',
   },
